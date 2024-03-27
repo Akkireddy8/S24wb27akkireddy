20607d0a73f57602264b28ac85b905897fba69ca
Akkireddy8 <156268619+Akkireddy8@users.noreply.github.com>
2024-03-26 22:51:57 -0500

    added gift

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Learning about pugs
 
 Render Link : https://s24wb27akkireddy.onrender.com
+
+### Class: Gift
+
+*Class Name:* Gift
+
+*Attributes:* 
+- gift_type (string) - Specifies the type or category of the gift.
+- gift_size (string) - Denotes the size of the gift.
+- gift_price (number) - Represents the price of the gift.
--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var giftRouter = require('./routes/gift');
+var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 var app = express();
 
 // view engine setup
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/gift', giftRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
