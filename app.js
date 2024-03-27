7b0f8bdb3c00535fdde60f7c51985ff318b3ccd4
Akkireddy8 <156268619+Akkireddy8@users.noreply.github.com>
2024-03-27 00:13:22 -0500

    bonus

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/gift', giftRouter);
 app.use('/grid', gridRouter);
+app.use('/pick', pickRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
